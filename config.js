5c7781e1c36fb350d53a420ed8e7828c6fe84b1d
vuonganha11-ctrl <vuonganha11@gmail.com>
2026-05-31 16:08:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
    Lấy ID trong URL Sheet: docs.google.com/spreadsheets/d/<ID>/edit
    Để trống = các trang hiển thị dữ liệu mẫu.
    ==================================================================== */
-window.TKS_SHEET_ID = "";
+window.TKS_SHEET_ID = "1Hk49-OXujQCHanrYUaAyhc9kDNAi6gYker1f1_44p2Q";
